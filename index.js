e436296324ff7d7e60c216f783d02ffac378346a
kaelaclement <stephanie.kaela@gmail.com>
2020-05-29 15:04:53 +0100

    get harry potter characters by house

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const hbs = require('express-handlebars');
 const path = require('path');
 const app = express();
+require('dotenv').config();
+
+const hpApi = require('./lib/harrypotter');
+const getCharacters = hpApi.getCharacters;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,6 +19,43 @@ app.set('view engine', '.hbs');
 app.get('/', (req, res) => {
 	res.render('index');
 });
+
+app.get('/harrypotter/:house', async (req, res) => {
+	let house;
+	let template;
+
+	switch (req.params.house) {
+		case 'hufflepuffs':
+			house = 'Hufflepuff';
+			template = req.params.house;
+			break;
+		case 'ravenclaws':
+			house = 'Ravenclaw';
+			template = req.params.house;
+			break;
+		case 'gryffindors':
+			house = 'Gryffindor';
+			template = req.params.house;
+			break;
+		case 'slytherins':
+			house = 'Slytherin';
+			template = req.params.house;
+			break;
+		default:
+			res.send('Error - that is not a harry potter house');
+			break;
+	}
+
+	let data = await getCharacters(house);
+
+	let students = data.map(student => {
+		return student.name;
+	});
+
+	res.render(template, { students });
+});
+
+
 
 app.listen(3000, () => {
 	console.log('listening on port 3000');
